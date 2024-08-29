d97eec8944a5c3057d31c7b49a99c48166d9dad4
顺 张 <1924194645@qq.com>
2024-08-29 22:23:02 +0800

    EntityFramWorkCore SqlLite 学习

---
+++ EFGetStarted/Entitys/Blog.cs
@@ -0,0 +1,8 @@
+﻿
+namespace EFGetStarted.Entity;
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string Url { get; set; } = default!;
+    public List<Post> Posts { get; } = new();
+}
